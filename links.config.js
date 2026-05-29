f2e5a1c8e690cbe04a1a8720a4677cb47b447d36
Joey Gamban <joeygg@niceday.co.kr>
2026-05-29 14:10:13 +0900

    Build image-map landing page

---
+++ links.config.js
@@ -0,0 +1,33 @@
+window.AP_GU_LINKS = {
+  signup: "signup.html",
+  apply: "signup.html",
+  kakao: "https://open.kakao.com/",
+  email: "apgujamsalon@gmail.com",
+  instagram: "www.instagram.com",
+  youtube: "www.youtube.com",
+  spotify: "www.spotify.com",
+  event: "www.instagram.com",
+  members: "www.instagram.com",
+  clips: {
+    1: {
+      title: "공연 실황 / Live Performance",
+      url: "https://www.youtube.com/watch?v=XFkzRNyygfk&list=RDXFkzRNyygfk&start_radio=1"
+    },
+    2: {
+      title: "합주 스케치 / Rehearsal Sketch",
+      url: "https://www.youtube.com/watch?v=q5qDyjJ7PTw&list=RDq5qDyjJ7PTw&start_radio=1"
+    },
+    3: {
+      title: "게더링 파티 / Gathering Party",
+      url: "https://www.youtube.com/watch?v=5A_Fzh9JYEs&list=RD5A_Fzh9JYEs&start_radio=1"
+    },
+    4: {
+      title: "클리닉 현장 / Clinic Session",
+      url: "https://www.youtube.com/watch?v=2J2_bkJ_UG4&list=RD2J2_bkJ_UG4&start_radio=1"
+    },
+    5: {
+      title: "멤버 인터뷰 / Member Interview",
+      url: "https://www.youtube.com/watch?v=gD4Yno_AI0U&list=RDgD4Yno_AI0U&start_radio=1"
+    }
+  }
+};
